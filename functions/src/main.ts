93079496b042d8c7d5c0290ddc728e05965397a7
nelsontky <nelson@ccb.wtf>
2021-09-19 22:01:18 +0000

    feat: allow CORS

--- functions/src/main.ts
+++ functions/src/main.ts
@@ -4,8 +4,10 @@ import { AppModule } from "./app.module";
 import { ExpressAdapter } from "@nestjs/platform-express";
 import * as express from "express";
 import * as functions from "firebase-functions";
+import * as cors from "cors";
 
 const server = express();
+server.use(cors({ origin: true }));
 
 async function bootstrap(expressInstance) {
   const app = await NestFactory.create(
