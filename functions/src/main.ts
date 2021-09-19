3e18955977decb67ea09f46e47b11e117dd3d848
nelsontky <nelson@ccb.wtf>
2021-09-19 16:35:37 +0000

    feat: add firebase deployment

---
+++ functions/src/main.ts
@@ -0,0 +1,24 @@
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+import { ExpressAdapter } from "@nestjs/platform-express";
+import * as express from "express";
+import * as functions from "firebase-functions";
+
+const server = express();
+
+async function bootstrap(expressInstance) {
+  const app = await NestFactory.create(
+    AppModule,
+    new ExpressAdapter(expressInstance)
+  );
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  return app.init();
+}
+
+bootstrap(server)
+  .then(() => console.log("Nest Ready"))
+  .catch((err) => console.error("Nest broken", err));
+
+export const api = functions.https.onRequest(server);
